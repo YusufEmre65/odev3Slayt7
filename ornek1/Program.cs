9803eca2846bfa61bda66ed3057bb1bc3bfa099b
Yusuf0665 <95577518+Yusuf0665@users.noreply.github.com>
2022-12-06 02:06:00 +0300

    Add project files.

---
+++ ornek1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek1
+{
+        class Deneme
+        {
+            public int a;
+            public int b;
+
+            public Deneme(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public static int Topla(int x, int y)
+            {
+                return x + y;
+            }
+        }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ornek1.Deneme d = new ornek1.Deneme(1, 2);
+        Console.WriteLine(d.a);
+        Console.WriteLine(ornek1.Deneme.Topla(2, 3));
+        Console.ReadLine();
+    }
+}
---
+++ ornek2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek2
+{
+    class Deneme
+    {
+        public Deneme() {
+            Console.WriteLine("Deneme 1");
+        }
+    }
+}
+namespace ornek21
+{
+    class Deneme
+    {
+        public Deneme()
+        {
+            Console.WriteLine("Deneme 2");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        ornek2.Deneme d1= new ornek2.Deneme();
+        ornek21.Deneme d2= new ornek21.Deneme();
+        Console.ReadLine();
+    }
+}
